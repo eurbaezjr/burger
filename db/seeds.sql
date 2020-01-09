cc99eb6cd636f9951bc56cac34097657a0196992
Eduardo Urbaez <eurbaezjr@gmail.com>
2020-01-08 23:10:51 -0800

    update

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name) VALUES ('Cheese Burger');
+INSERT INTO burgers (burger_name) VALUES ('Chicken Sandwich');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Chili Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Buffalo Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Blue Cheese Burger', true);
